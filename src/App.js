fffdacebda010302a7a58d334143766a12e7afc7
ega1231 <ega.munkhbat@gmail.com>
2022-09-10 14:02:33 -0700

    Added Expenditure data model

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 
+class HouseholdInfo {
+  constructor({married, numChildren, zipCode, income}) {
+    this.married = married;
+    this.numChildren = numChildren;
+    this.zipCode = zipCode;
+    this.income = income;
+  }
+}
+class ExpenditureInfo{
+  constructor({new_clean_vehicle_classification,
+  new_clean_vehicle_msrp,
+  purchased_qualifying_new_clean_vehicle}){
+    this.new_clean_vehicle_classification = new_clean_vehicle_classification;
+    this.new_clean_vehicle_msrp = new_clean_vehicle_msrp;
+    this.purchased_qualifying_new_clean_vehicle = purchased_qualifying_new_clean_vehicle;
+  }
+}
 function App() {
+  const [household, setHousehold] = useState(new HouseholdInfo({
+    married: true,
+    numChildren: 3,
+    zipCode: 98102,
+    income: 50000,
+  }));
   return (
     <div className="App">
       <header className="App-header">
